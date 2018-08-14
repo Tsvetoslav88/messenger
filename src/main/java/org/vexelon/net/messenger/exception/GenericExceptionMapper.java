a065fb1fbd25c46ee5d980ffb72c6dd702848cdb
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-08-14 23:06:35 +0300

    Add WebApplicationException

--- src/main/java/org/vexelon/net/messenger/exception/GenericExceptionMapper.java
+++ src/main/java/org/vexelon/net/messenger/exception/GenericExceptionMapper.java
@@ -7,7 +7,7 @@ import javax.ws.rs.ext.Provider;
 
 import org.vexelon.net.messenger.model.ErrorMessage;
 
-@Provider
+//@Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable>{
 
 	@Override
